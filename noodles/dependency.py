bfc6265a70b7b7cab750e376d7904bd2aedd246a
Badi' Abdul-Wahid <badi@iu.edu>
2015-04-08 12:29:28 -0400

    add unit test for dependency graph

--- noodles/dependency.py
+++ noodles/dependency.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import unittest
 
 
 class EdgeLabels:
@@ -71,3 +72,80 @@ class Dependencies(object):
 
     def to_agraph(self):
         return nx.to_agraph(self._G)
+
+
+class TestDependencies(unittest.TestCase):
+    def setUp(self):
+        self.G = Dependencies()
+        self.r = 'test_root'
+        self.child_template = 'test_node_{}'
+        self.counter = 0
+
+    def new_child(self):
+        name = self.child_template.format(self.counter)
+        self.counter += 1
+        return name
+
+    @property
+    def c(self):
+        "child name"
+        return self.new_child()
+
+    def node_color(self, name, color):
+        self.assertEqual(self.G.node[name]['color'], color)
+
+    def edge_color(self, s, t, c):
+        self.assertEqual(self.G.edge[s][t]['color'], c)
+
+    def is_node(self, name):
+        self.assertIn(name, self.G.nodes())
+
+    def is_root(self, name):
+        self.is_node(name)
+        self.node_color(name, Color.ROOT)
+
+    def is_module(self, name):
+        self.is_node(name)
+        self.node_color(name, Color.MODULE)
+
+    def is_attribute(self, name):
+        self.is_node(name)
+        self.node_color(name, Color.ATTRIBUTE)
+
+    def is_edge(self, s, t):
+        self.assertIn((s, t), self.G.edges())
+
+    def test_add_root(self):
+        name = self.r
+        self.G.add_root(name)
+        self.is_root(name)
+
+    def test_add_module(self):
+        name = self.c
+        self.G.add_module(name)
+        self.is_module(name)
+
+    def test_add_submodule(self):
+        root = self.r
+        child = self.c
+        self.G.add_submodule(root, child)
+        self.is_node(root)
+        self.is_module(child)
+        self.is_edge(root, child)
+        self.edge_color(root, child, Color.IMPORT)
+
+    def test_add_attribute(self):
+        name = self.c
+        self.G.add_attribute(name)
+        self.is_node(name)
+        self.is_attribute(name)
+
+    def test_add_subattribute(self):
+        root = self.r
+        child = self.c
+        self.G.add_subattribute(root, child)
+        self.is_node(root)
+        self.is_attribute(child)
+        self.is_edge(root, child)
+        self.edge_color(root, child, Color.ATTRIBUTE)
+
