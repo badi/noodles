954642210cd9ac45087121b9a97e1d9f8db6da82
Badi' Abdul-Wahid <badi@iu.edu>
2015-04-08 11:55:55 -0400

    refactor version

--- noodles/__init__.py
+++ noodles/__init__.py
@@ -1,14 +1,6 @@
 
-try:
-    from version import version
-except ImportError:
-    import sys
-    sys.stderr.write('Unable to import version\n')
-    version = 'UNKNOWN'
-
-
+from version import version
 from . import main
 from . import imports
 from . import requirements
-
 
