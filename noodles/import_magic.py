bb541f1e4efe128db3651f61a2b691cf7b18b265
Badi' Abdul-Wahid <badi@iu.edu>
2015-04-08 15:20:35 -0400

    some import magic

---
+++ noodles/import_magic.py
@@ -0,0 +1,62 @@
+import imp
+
+import types
+import unittest
+
+
+def _find_and_load_single_module(name, path):
+    """Find and load a single module using `imp.find_module` and `imp.load_module`
+
+    :param name: name of the module :: str
+    :param path: paths to search in :: str or None
+    :returns: the module
+    :rtype: `module`
+    """
+
+    try:
+        fileobj, path, descr = imp.find_module(name, path)
+
+        if path is not None:
+            return imp.load_module(name, fileobj, path, descr)
+
+    finally:
+        try:
+            if fileobj is not None:
+                fileobj.close()
+        except UnboundLocalError:
+            pass
+
+
+def find_module(name, path=None):
+    """Try to find the module name. This is a wrapper over the builtin
+    imp.find_module but handles hierarchical module names.
+
+    :param name: the name of the module
+    :param path: optional path to search in
+    :returns: path to the module
+    :rtype: str
+    """
+
+    names = name.split('.')
+    module = imp.new_module('dummy_module')
+
+    for n in names:
+        if hasattr(module, n):
+            module = getattr(module, n)
+        else:
+            module = _find_and_load_single_module(n, path)
+
+        if hasattr(module, '__path__'):
+            path = module.__path__
+
+    return module
+
+
+class TestImportMagic(unittest.TestCase):
+    def test_find_simple(self):
+        mod = find_module('os')
+        self.assertIsInstance(mod, types.ModuleType)
+
+    def test_find_hierarchical(self):
+        mod = find_module('os.path')
+        self.assertIsInstance(mod, types.ModuleType)
