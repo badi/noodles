0ba7f7c6f4f3f92393f9a00499b9204db930f3b5
Badi' Abdul-Wahid <badi@iu.edu>
2015-04-08 12:56:07 -0400

    generate a graph from requirements

--- noodles/requirements.py
+++ noodles/requirements.py
@@ -9,6 +9,8 @@ import tempfile
 import textwrap
 import unittest
 
+from .dependency import Dependencies
+
 
 def parse_requirements_file(path):
     """Parse the requirements.txt
@@ -44,6 +46,24 @@ def _create_requirements_file(content):
     return tmp
 
 
+def graph_requirements(root_name, path):
+    """Graph the import dependencies of a package
+
+    :param root_name: the name of the package
+    :param path: path to the requirements file
+    :returns: dependency graph
+    :rtype: `noodles.dependency.Dependencies`
+
+    """
+    G = Dependencies()
+    G.add_root(root_name)
+
+    for r in parse_requirements_file(path):
+        G.add_sublibrary(root_name, r)
+
+    return G
+
+
 class TestRequirements(unittest.TestCase):
 
     def setUp(self):
@@ -72,6 +92,19 @@ class TestRequirements(unittest.TestCase):
 
         for e in self.expected:
             self.assertIn(e, actual)
+
+        self.tmp.seek(0)
+
+    def test_graph_requirements(self):
+        root_name = 'test_root'
+
+        G = graph_requirements(root_name, self.tmp.name)
+        self.tmp.seek(0)
+
+        self.assertIn(root_name, G.nodes())
+        for e in self.expected:
+            self.assertIn(e, G.nodes())
+            self.assertIn((root_name, e), G.edges())
 
 if __name__ == '__main__':
     unittest.main()
